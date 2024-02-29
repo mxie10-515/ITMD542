df6e0472d53a8ba2b29524d5d3921abfa32b5841
mxie10@hawk.iit.edu <mxie10@hawk.iit.edu>
2024-02-29 14:18:23 -0600

    update index.js

--- contactappwithSQLite/routes/index.js
+++ contactappwithSQLite/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'This is a WebPage for CRUD operation of Contact' });
+  res.render('index', { title: 'This is a WebPage for CRUD operation of Contact using SQLite' });
 });
 
 module.exports = router;
